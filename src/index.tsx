ac01e71a36d0350542eaba66af90247ed3609f96
Popova Julia <yuliapopova.s@yandex.ru>
2024-03-14 16:11:56 +0300

    added alias

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import { createRoot } from 'react-dom/client';
 import { App } from './components/App';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import { About } from './pages/about';
-import { Shop } from './pages/shop';
+import { About } from '@/pages/about';
+import { Shop } from '@/pages/shop';
 import { Suspense } from 'react';
 
 const root = document.getElementById('root');
